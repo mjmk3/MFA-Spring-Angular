fef6d1d65658f489176bd45eec304688c6067440
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-21 20:33:25 +0300

    MFA

---
+++ src/main/java/app/mfa/mfaspring/MfaSpringApplication.java
@@ -0,0 +1,13 @@
+package app.mfa.mfaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/mfa/mfaspring/MfaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mfa.mfaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
